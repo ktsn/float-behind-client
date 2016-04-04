2357ed937771dec687016abb5cb9dda46b97f453
ktsn <ktsn55@gmail.com>
2016-04-04 22:00:45 +0900

    DRY webpack.config.dev

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,40 +1,6 @@
 /* eslint-env node */
-const webpack = require('webpack');
-const path = require('path');
-const autoprefixer = require('autoprefixer');
+const config = require('./webpack.config');
 
-const env = require('dotenv').config();
+config.cache = config.debug = config.watch = true;
 
-module.exports = {
-  context: path.resolve(__dirname, 'app'),
-  entry: './main.js',
-  output: {
-    path: path.resolve(__dirname, '.tmp'),
-    filename: './main.js'
-  },
-  resolve: {
-    moduleDirectories: ['bower_components', 'node_modules'],
-    extensions: ['', '.js']
-  },
-  cache: true,
-  debug: true,
-  devtool: 'source-map',
-  module: {
-    loaders: [
-      {test: /\.html$/, loader: 'html-loader?attrs=false'},
-      {test: /\.scss$/, loader: 'style!css!postcss!sass?sourceMap'},
-      {test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/}
-    ]
-  },
-  postcss: function() {
-    return [autoprefixer];
-  },
-  externals: {
-    'google-analytics': 'var window.ga'
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.GA_ID': JSON.stringify(env.GA_ID)
-    })
-  ]
-};
+module.exports = config;
