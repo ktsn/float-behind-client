b230fa9d6d16df8a1207d1fbd6edc1cbc5cedd72
ktsn <ktsn55@gmail.com>
2016-05-01 00:31:46 +0900

    Apply vue-router and add intro page

--- app/main.js
+++ app/main.js
@@ -2,18 +2,36 @@ import './main.scss';
 
 import Vue from 'vue';
 import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
 import Helpers from './plugins/helpers';
+
+// Components
 import cardWrapper from './components/card-wrapper/card-wrapper';
+import intro from './components/intro/intro';
 
 import { pageView } from './utils/ga';
 
 Vue.use(VueResource);
+Vue.use(VueRouter);
 Vue.use(Helpers);
 
 Vue.http.options.root = '/api/v1';
 
-const app = new Vue(cardWrapper);
+const App = Vue.extend({});
 
-app.$mount('#app');
+const router = new VueRouter({
+  history: process.env.NODE_ENV === 'production'
+});
+
+router.map({
+  '/': {
+    component: cardWrapper
+  },
+  '/intro': {
+    component: intro
+  }
+});
+
+router.start(App, '#app');
 
 pageView();
