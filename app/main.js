b9f48ed21c3d368c4f19ad632bff46ebcc0a3e98
ktsn <ktsn55@gmail.com>
2017-01-10 21:56:11 +0900

    Remove vue-resource

--- app/main.js
+++ app/main.js
@@ -1,7 +1,6 @@
 import './main.scss'
 
 import Vue from 'vue'
-import VueResource from 'vue-resource'
 import VueRouter from 'vue-router'
 import Helpers from './plugins/helpers'
 
@@ -13,7 +12,6 @@ import intro from './components/intro/intro'
 
 import { pageView } from './utils/ga'
 
-Vue.use(VueResource)
 Vue.use(VueRouter)
 Vue.use(Helpers)
 
